03433d014091f8e1ecae33e0fb554c4f74fd8bba
曹云友 <caoyunyou@foxmail.com>
2025-05-13 21:32:00 +0800

    add 添加注释信息

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,10 +8,13 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+// gradle 构建时注释下面的subprojects，项目调试和启动时需要恢复
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
